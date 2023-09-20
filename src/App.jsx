9305838792884f0096aaac3c953da88197dbe044
Maya Clinton <mayak.clinton@gmail.com>
2023-09-20 19:14:42 +0100

    migrating to vite from create-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 //import "./styles/styles.css";
 
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 
 
 export default function App() {
